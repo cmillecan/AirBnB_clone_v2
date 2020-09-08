ce37cd52b5cceadfb0976c5d4fabd7a7bd0449c1
cmillecan <cmillecan@gmail.com>
2020-09-08 23:31:02 +0000

    update file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,7 @@ class FileStorage:
             del self.__objects[key]
         except:
             pass
+
+    def close(self):
+        """deserializing the JSON file to objects"""
+        self.reload()
