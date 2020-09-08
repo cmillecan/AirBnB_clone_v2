f2a536acfb03edbe908667ba054059371b4a1867
cmillecan <cmillecan@gmail.com>
2020-09-08 23:42:08 +0000

    add files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,7 +7,13 @@ import models
 import sys
 from os import getenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class DBStorage:
